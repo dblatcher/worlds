6a69c53740635b8e03831ad81b070e4e01049c36
David Blatcher <david.blatcher@graduate-jobs.com>
2021-02-06 14:39:19 +0000

    use ticksPerSecond property on World

--- src/World.ts
+++ src/World.ts
@@ -4,10 +4,12 @@ class World {
     gravity: number
     things: Thing[]
     timer: NodeJS.Timeout
-    canvas? :HTMLCanvasElement
+    canvas?: HTMLCanvasElement
+    timerSpeed: number
     constructor(gravity: number, things: Thing[]) {
         this.gravity = gravity
         this.things = things
+        this.timerSpeed = 0
 
         things.forEach(thing => {
             thing.world = this
@@ -15,23 +17,39 @@ class World {
     }
 
     get report() {
-        return `The local gravity is ${this.gravity}. There are ${this.things.length} things.`
+        return `The local gravity is ${this.gravity}. Time runs at ${this.ticksPerSecond} hertz. There are ${this.things.length} things.`
     }
 
     tick() {
-        this.things.forEach(thing => {
-            thing.move()
-        })
+        this.things.forEach(thing => { thing.move() })
         this.renderOnCanvas()
     }
 
-    startTime(speed: number) {
+    set ticksPerSecond(speed: number) {
+        if (speed == 0) {
+            clearInterval(this.timer)
+            this.timerSpeed = 0
+            return
+        }
+
+        if (this.timerSpeed !== 0) { clearInterval(this.timer) }
         const tick = this.tick.bind(this)
-        this.timer = setInterval(tick, speed)
+        this.timerSpeed = speed
+        this.timer = setInterval(tick, 1000 / speed)
+    }
+
+    get ticksPerSecond() {
+        return this.timerSpeed
+    }
+
+    stopTime() {
+        clearInterval(this.timer)
+        this.timerSpeed = 0
+        return this
     }
 
     renderOnCanvas() {
-        if (!this.canvas) {return false}
+        if (!this.canvas) { return false }
         const ctx = this.canvas.getContext("2d");
 
         ctx.fillStyle = "black";
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,14 @@ import { World } from './World'
 import { Thing } from './Thing'
 import { Force } from './Force'
 
+const divElement = document.createElement('div')
+document.body.appendChild(divElement);
+
+const canvasElement = document.createElement('canvas')
+canvasElement.setAttribute('height', '1000');
+canvasElement.setAttribute('width', '1000');
+document.body.appendChild(canvasElement);
+
 const world = new World(.01, [
     new Thing({ x: 500, y: 400, size: 60, density: 20 }),
     new Thing({ x: 40, y: 120, size: 15, density: 10, color: 'blue' }, new Force(1, .5)),
@@ -9,16 +17,8 @@ const world = new World(.01, [
     // new Thing({x:60, y:120, size:7, color:'blue', heading: .6}),
 ]);
 
-const divElement = document.createElement('div')
-divElement.innerText = world.report
-document.body.appendChild(divElement);
-
-const canvasElement = document.createElement('canvas')
-canvasElement.setAttribute('height', '1000');
-canvasElement.setAttribute('width', '1000');
-
-
-document.body.appendChild(canvasElement);
 
 world.canvas = canvasElement
-world.startTime(50)
+world.ticksPerSecond = 50
+divElement.innerText = world.report
+globalThis.world = world
