8bd273b3905ec60b718407567fc38b705b26392d
David Blatcher <david.blatcher@graduate-jobs.com>
2021-02-13 17:40:23 +0000

    adjust galaxy world

--- src/preset-worlds/galaxy.ts
+++ src/preset-worlds/galaxy.ts
@@ -3,10 +3,18 @@ import { LinedThing, Thing } from '../Thing'
 import { Force } from '../Force'
 
 
-const redPlanet = new Thing({ x: 700, y: 100, size: 10, density: 5, color: 'red', elasticity: .8 })
-const whiteStar = new LinedThing({ x: 500, y: 500, size: 70, density: 10, immobile:true })
-const bluePlanet= new Thing({ x: 200, y: 730, size: 18, density: 2, color: 'blue', elasticity: .7 }, new Force(24, Math.PI * (0.5)))
-const pinkPlanet = new Thing({ x: 700, y: 700, size: 15, density: 3, color: 'pink', elasticity: .25 }, new Force(8, Math.PI * (1)))
+const whiteStar = new LinedThing({ x: 500, y: 500, size: 10, density: 150, immobile:true })
+const redPlanet = new Thing({ 
+    x: 300, y: 600, size: 10, density: 5, color: 'red', elasticity: .8 
+}, new Force(24, Math.PI * 0.5))
+
+const bluePlanet= new Thing({ 
+    x: 200, y: 730, size: 18, density: 2, color: 'blue', elasticity: .7 
+}, new Force(24, Math.PI * 0.5))
+
+const pinkPlanet = new Thing({ 
+    x: 200, y: 800, size: 15, density: 3, color: 'pink', elasticity: .25 
+}, new Force(10, Math.PI * 0.5))
 
 
 const galaxy = new World([
