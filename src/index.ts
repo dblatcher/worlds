5c851874351b7e8974b857081620e097f3adc8ad
David Blatcher <david.blatcher@graduate-jobs.com>
2021-02-14 12:29:57 +0000

    center canvas on index page

--- src/index.ts
+++ src/index.ts
@@ -10,20 +10,35 @@ const canvasElement = document.createElement('canvas')
 
 const styleSheet = document.createElement('style')
 styleSheet.textContent = `
+.frame {
+    width: 100%;
+    height: 100%;
+    max-height: 95vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    z-index: -1;
+}
+
 canvas {
-    max-width: 100%;
-    height: auto;
     border: 20px outset gray;
     box-sizing: border-box;
+    max-height: inherit;
 }
 `
 
 const panel = new WorldControlPanel(balance, { worldOptions: [balance, galaxy, rocksAndBallons, testWorld] })
 const shipPanel = new SpaceShipControlPanel(myShip)
 
-panel.world.setCanvas (canvasElement)
+panel.world.setCanvas(canvasElement)
 
 document.head.appendChild(styleSheet)
 document.body.appendChild(shipPanel.makeElement())
 document.body.appendChild(panel.makeElement())
-document.body.appendChild(canvasElement);
+
+const frame = document.createElement('div')
+frame.classList.add('frame')
+
+frame.appendChild(canvasElement);
+document.body.appendChild(frame);
